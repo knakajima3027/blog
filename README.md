8e5c0d563484891b7bf2917a10c0a0bfe3192847
kNakajima3027 <kamihate1012@gmail.com>
2019-02-02 15:34:34 +0900

    add README and setpu.sh

---
+++ README.md
@@ -0,0 +1,5 @@
+## メモ  
+### cloneするとき  
+`git clone https://github.com/knakajima3027/blog`
+`cd blog`
+`sh setup.sh
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+#download blog in public
+git clone https://github.com/knakajima3027/knakajima3027.github.io public/
+
+# update shemes
+cd themes
+
+git pull
